1e7aed0ceff5fbc46b2a4b6e261d16a098ddbb4c
Rickey Rodent <blgold567@gmail.com>
2024-05-10 16:48:45 +0800

    HABOL PA 2 ELECTRIC BOOGALOO

--- src/MCQ.java
+++ src/MCQ.java
@@ -12,7 +12,7 @@ public class MCQ {
           new questionConstructor(QuC.q5, QuC.choice5, "c"),
           new questionConstructor(QuC.q6, QuC.choice6, "b"),
           new questionConstructor(QuC.q7, QuC.choice7, "c"),
-          new questionConstructor(QuC.q8, QuC.choice8,"d" ),
+          new questionConstructor(QuC.q8, QuC.choice8,"c" ),
           new questionConstructor(QuC.q9, QuC.choice9, "a"),
           new questionConstructor(QuC.q10, QuC.choice10, "d"),
           new questionConstructor(QuC.q11, QuC.choice11, "b"),
@@ -25,46 +25,68 @@ public class MCQ {
     }
     //Test Proper
     public static void doTest(questionConstructor[] questions){
-        int score = 0, questNum = 1;
+        int attemptNum = 1;
+        boolean retakeFromTop = false;
+        while(!retakeFromTop) {
+            int score = 0, questNum = 1;
+            String tstName, retake;
 
-        String tstName;
+            System.out.println("============================================");
+            System.out.println("|                                          |");
+            System.out.println("|          PHILIPPINE HISTORY TEST         |");
+            System.out.println("|                                          |");
+            System.out.println("============================================");
 
-        System.out.println("============================================");
-        System.out.println("|                                           |");
-        System.out.println("|          PHILIPPINE HISTORY TEST          |");
-        System.out.println("|                                           |");
-        System.out.println("============================================");
-
-        Scanner usrInput = new Scanner(System.in);
-
-        System.out.println("Please Answer the Test with small letters only, if you've answered a Big letter, the test will consider it a mistake");
-        System.out.println("Press Enter to Continue..........");
-        usrInput.nextLine();
-
-        do {
-            System.out.print("Please Enter your Name: ");
-            tstName = usrInput.nextLine();
-        }while (tstName.isEmpty());
-
-        System.out.println("************** TEST PROPER ******************");
-        for (questionConstructor question : questions) {
-            System.out.println("Question "+ questNum +": " + question.question);
-            System.out.println(question.choices);
-            System.out.print("Your Answer?: ");
-            String answer = usrInput.next();
-            questNum++;
-            if (answer.equals(question.answer)) {
-                score++;
+            if(attemptNum > 1){
+                System.out.println("|      ATTEMPT "+attemptNum +"      |");
             }
-        }
-        usrInput.close();
-        System.out.println("************** END OF TEST ******************");
-        if(score >= 8){
-            System.out.println("Thank you " +tstName + " for taking the test, and Congratulations you have PASSED the test with a score of " +score+ " out of 15");
-        }else {
-            System.out.println("Thank you " +tstName + " for taking the test, but unfortunately you have FAILED the test with a score of " +score+ " out of 15, better luck next time!");
-        }
 
+            Scanner usrInput = new Scanner(System.in);
+
+            System.out.println("Please Answer the Test with small letters only, if you've answered a Big letter, the test will consider it a mistake");
+            System.out.println("Press Enter to Continue..........");
+            usrInput.nextLine();
+
+            do {
+                System.out.print("Please Enter your Name: ");
+                tstName = usrInput.nextLine();
+            } while (tstName.isEmpty());
+
+                System.out.println("************** TEST PROPER ******************");
+                for (questionConstructor question : questions) {
+                    System.out.println("Question " + questNum + ": " + question.question);
+                    System.out.println(question.choices);
+                    System.out.print("Your Answer?: ");
+                    String answer = usrInput.next();
+                    questNum++;
+                    if (answer.equals(question.answer)) {
+                        score++;
+                    }
+                }
+
+                System.out.println("************** END OF TEST ******************");
+                if (score >= 8) {
+                    System.out.println("Thank you " + tstName + " for taking the test, and Congratulations you have PASSED the test with a score of " + score + " out of 15");
+                } else {
+                    System.out.println("Thank you " + tstName + " for taking the test, but unfortunately you have FAILED the test with a score of " + score + " out of 15, better luck next time!");
+                }
+                System.out.println("Please press enter to continue........");
+                usrInput.nextLine();
+                usrInput.nextLine();
+                System.out.print("Do you want to retake the test again? (y/n): ");
+                retake = usrInput.nextLine();
+
+                if(retake.compareToIgnoreCase("n") == 0){
+                    retakeFromTop = true;
+                    System.out.println("Thank you for taking our test");
+                    usrInput.close();
+                    continue;
+
+                }
+                attemptNum++;
+
+
+        }
 
     }
 }
